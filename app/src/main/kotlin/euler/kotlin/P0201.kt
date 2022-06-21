02d7cb03fb2d54b0d8853f0b387b39158e5e8a67
Xuemin Guan <gxuemin@gmail.com>
2022-06-21 11:59:56 +0100

    Removed problem 201 as I couldn't come up a solution and I had to google its solution.

--- app/src/main/kotlin/euler/kotlin/P0201.kt
+++
@@ -1,9 +0,0 @@
-package euler.kotlin
-
-/*Subsets with a unique sum*/
-
-class P0201(private val numberSet: Set<Int>) {
-    fun solution(numOfValuesInSubset: Int): Int {
-        return numberSet.sum()
-    }
-}
